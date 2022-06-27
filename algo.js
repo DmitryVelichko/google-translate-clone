8494821419c9bb6b71ec37367d0f994bd17109a3
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-27 23:03:27 +0300

    new commit

---
+++ algo.js
@@ -0,0 +1,14 @@
+import Comparator from '../../utils/comparator/Comparator';
+
+/**
+ * Parent class for Min and Max Heaps.
+ */
+export default class Heap {
+  /**
+   * @constructs Heap
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    if (new.target === Heap) {
+      throw new TypeError('Cannot construct Heap instance directly');
+    }
