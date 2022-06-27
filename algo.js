be96171971eed31a02bd63477f9a7c308e566ac4
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-27 23:03:35 +0300

    new commit

--- algo.js
+++ algo.js
@@ -12,3 +12,16 @@ export default class Heap {
     if (new.target === Heap) {
       throw new TypeError('Cannot construct Heap instance directly');
     }
+
+    // Array representation of the heap.
+    this.heapContainer = [];
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  /**
+   * @param {number} parentIndex
+   * @return {number}
+   */
+  getLeftChildIndex(parentIndex) {
+    return (2 * parentIndex) + 1;
+  }
