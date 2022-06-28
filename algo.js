96696c16a55e2630a91e6a3389359b3b3b3ba2b7
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-28 15:39:21 +0300

    new commit

--- algo.js
+++ algo.js
@@ -3,3 +3,7 @@ import Comparator from '../../utils/comparator/Comparator';
 /**
  * @typedef {Object} SorterCallbacks
  * @property {function(a: *, b: *)} compareCallback - If provided then all elements comparisons
+ *  will be done through this callback.
+ * @property {function(a: *)} visitingCallback - If provided it will be called each time the sorting
+ *  function is visiting the next element.
+ */
