17a9253bbc3fb886745de289ae2c054f96d9d344
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-29 16:51:15 +0300

    new commit

--- algo.js
+++ algo.js
@@ -4,3 +4,5 @@ export default class GraphEdge {
    * @param {GraphVertex} endVertex
    * @param {number} [weight=1]
    */
+  constructor(startVertex, endVertex, weight = 0) {
+    this.startVertex = startVertex;
