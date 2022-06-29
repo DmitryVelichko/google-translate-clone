767b5c1de6ba0053ddb86e28e0ee397c3065fe00
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-29 16:53:12 +0300

    new commit

--- algo.js
+++ algo.js
@@ -8,3 +8,12 @@ export default class GraphVertex {
     if (value === undefined) {
       throw new Error('Graph vertex must have a value');
     }
+
+    /**
+     * @param {GraphEdge} edgeA
+     * @param {GraphEdge} edgeB
+     */
+    const edgeComparator = (edgeA, edgeB) => {
+      if (edgeA.getKey() === edgeB.getKey()) {
+        return 0;
+      }
