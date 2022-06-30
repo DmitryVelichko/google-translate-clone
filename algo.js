3e5661d926879614c025153edbe16f14f386ad4a
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-30 23:34:47 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,45 +1,6 @@
-import LinkedList from '../linked-list/LinkedList';
+import * as mtrx from '../../math/matrix/Matrix';
 
-export default class GraphVertex {
-  /**
-   * @param {*} value
-   */
-  constructor(value) {
-    if (value === undefined) {
-      throw new Error('Graph vertex must have a value');
-    }
+// The code of an 'A' character (equals to 65).
+const alphabetCodeShift = 'A'.codePointAt(0);
+const englishAlphabetSize = 26;
 
-    /**
-     * @param {GraphEdge} edgeA
-     * @param {GraphEdge} edgeB
-     */
-    const edgeComparator = (edgeA, edgeB) => {
-      if (edgeA.getKey() === edgeB.getKey()) {
-        return 0;
-      }
-
-      return edgeA.getKey() < edgeB.getKey() ? -1 : 1;
-    };
-
-    // Normally you would store string value like vertex name.
-    // But generally it may be any object as well
-    this.value = value;
-    this.edges = new LinkedList(edgeComparator);
-  }
-
-  /**
-   * @param {GraphEdge} edge
-   * @returns {GraphVertex}
-   */
-  addEdge(edge) {
-    this.edges.append(edge);
-
-    return this;
-  }
-
-  /**
-   * @param {GraphEdge} edge
-   */
-  deleteEdge(edge) {
-    this.edges.delete(edge);
-  }
