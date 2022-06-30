6da747b6602a810a77ae9c1207d9423ea43082a5
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-30 23:34:57 +0300

    new commit

--- algo.js
+++ algo.js
@@ -4,3 +4,9 @@ import * as mtrx from '../../math/matrix/Matrix';
 const alphabetCodeShift = 'A'.codePointAt(0);
 const englishAlphabetSize = 26;
 
+/**
+ * Generates key matrix from given keyString.
+ *
+ * @param {string} keyString - a string to build a key matrix (must be of matrixSize^2 length).
+ * @return {number[][]} keyMatrix
+ */
