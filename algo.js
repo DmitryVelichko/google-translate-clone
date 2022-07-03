c6850d121b98dc1378f4674c4b74b524e2422107
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-03 23:50:53 +0300

    new commit

--- algo.js
+++ algo.js
@@ -6,3 +6,7 @@
  export default function levenshteinDistance(a, b) {
   // Create empty edit distance matrix for all possible modifications of
   // substrings of a to substrings of b.
+  const distanceMatrix = Array(b.length + 1).fill(null).map(() => Array(a.length + 1).fill(null));
+
+  // Fill the first row of the matrix.
+  // If this is first row then we're transforming empty string to a.
