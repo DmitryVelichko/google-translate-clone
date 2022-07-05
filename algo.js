e78a5c14cadfd95a1292093da3a7e219174c0f3e
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-05 23:36:52 +0300

    new commit

--- algo.js
+++ algo.js
@@ -2,3 +2,6 @@ export default class Stack {
   constructor() {
     // We're going to implement Stack based on LinkedList since these
     // structures are quite similar. Compare push/pop operations of the Stack
+    // with prepend/deleteHead operations of LinkedList.
+    this.linkedList = new LinkedList();
+  }
