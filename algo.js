e96afe85e1f4af7e5745aae7fbe1a10059ec2204
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-08 22:40:46 +0300

    new commit

--- algo.js
+++ algo.js
@@ -5,10 +5,10 @@ let end = arr.length - 1;
 let target = 4;
 
 function binarySearchRecursive(arr, start, end, target) {
-  if(start > end) return false;
-  let midIndex = Math.floor((start + end) /2);
-  if(arr[midIndex] === target) return true;
-  if(arr[midIndex > target]) {
+  if (start > end) return false;
+  let midIndex = Math.floor((start + end) / 2);
+  if (arr[midIndex] === target) return true;
+  if (arr[midIndex > target]) {
     return binarySearchRecursive(arr, start, midIndex - 1, target);
   } else {
     return binarySearchRecursive(arr, midIndex + 1, end, target);
