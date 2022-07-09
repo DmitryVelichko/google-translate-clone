13f462e3fd5fbbcedca50393888cdd4e1bfcc6c0
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-09 23:52:19 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,5 +1,7 @@
-const arr = [1,4,6,3,5,7,32,54,42,234];
+const arr = [1, 4, 6, 3, 5, 7, 32, 54, 42, 234];
 
 const selectionSort = (arr) => {
-
-}
+  for (let i = 0; i < arr.length; i++) {
+    
+  }
+};
