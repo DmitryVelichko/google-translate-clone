e90bbf0928aab22dec836281fbc9f9e141cff235
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-10 11:13:02 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,8 +1,18 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 19];
 
 const binarySearch = (arr, value) => {
-  for (let i = 0; i < arr.length; i++) {
-    
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (arr[middle] === value) return middle;
+    if (arr[middle] < value) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
+    }
   }
+  return -1;
 };
 console.log(selectionSort(arr, 7));
